906e48ea6e94be98e978b1991d9560131659f5a1
Kurumii <orionhsu5@hotmail.com>
2023-02-13 12:21:53 -0500

    base pygame functionality completed

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# twitch-flying-chat-overlay-pygame
+# Twitch Flying Chat Overlay Pygame v0.4
+
+Insert gif here
+
+A streaming overlay to display chat that scrolls horizontally, similar to niconico and bilibili, built with Pygame
+
+Instructions:
+
+- `cd` into this directory
+- `python flying_text.py`
+
+In OBS:
+- Add a **Window Capture** source to your scene
+- Name it whatever
+- For **Window**, select `pygame window`
+- For **Capture Method**, select `Windows 10`
+- Right click on your **Window Capture** under `Sources` and select `Filters`
+- Add a **Chroma Key** filter
+- Select the filter and select `Custom` for the **Key Color Type**
+- Press **Select Color** and type `#000000` under **HTML**
+- Drag the **Similarity** bar all the way to the left (value of 1)
+- Don't forget to drag your window (under `Sources`) all the way to the top of the list
---
+++ flying_text.py
@@ -0,0 +1,99 @@
+import pygame
+import sys
+import random
+
+import asyncio
+import socketio
+
+sio = socketio.AsyncClient()
+
+chat_arr = []
+
+WIDTH = 1600
+HEIGHT = 900
+
+class Text():
+    def __init__(self, text, color, start_time, end_time, y_position, x_increment):
+        self.text = text
+        self.color = color
+        self.start_time = start_time
+        self.end_time = end_time
+        self.y_position = y_position
+        self.x_increment = x_increment
+
+def append_text(text, arr):
+    color = (255, 0, 0)
+    start_time = pygame.time.get_ticks()
+    end_time = pygame.time.get_ticks() + 10000 # temp solution
+    y_position = random.randint(0, HEIGHT - 50)
+    x_increment = random.randint(13, 17)
+    arr.append(Text(text, color, start_time, end_time, y_position, x_increment))
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+@sio.event
+async def data(data):
+    print(f'message received with {data}')
+
+    append_text(data["message"], chat_arr)
+
+async def sio_main():
+    await sio.connect('http://localhost:3001/')
+    print("hi")
+    await sio.wait()
+    print("lol")
+
+def main():
+    pygame.init()
+
+    font = pygame.font.SysFont('Arias', 30)
+
+    target_fps = 60
+    fps_clock = pygame.time.Clock()
+
+    screen = pygame.display.set_mode([WIDTH, HEIGHT], flags=pygame.NOFRAME)
+    screen.fill((0, 0, 0))
+
+    append_text("Hello World", chat_arr)
+    append_text("Hi Haha", chat_arr)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((0, 0, 0))
+
+        cur_time = pygame.time.get_ticks()
+
+        # updates all the text
+        for chat in chat_arr:
+            text_string = chat.text
+            text_color = chat.color
+            text_x = WIDTH - ((cur_time - chat.start_time) / target_fps) * chat.x_increment
+            text_y = chat.y_position
+            text_surface = font.render(text_string, False, text_color)
+            screen.blit(text_surface, (text_x, text_y))
+
+        # removes unnecessary items from array
+        chat_arr[:] = [chat for chat in chat_arr if cur_time <= chat.end_time]
+
+        # testing
+        if cur_time % 170 == 0:
+            append_text("This is a test message that is being shown", chat_arr)
+
+        pygame.display.update()
+        fps_clock.tick(target_fps)
+
+if __name__ == '__main__':
+    asyncio.run(sio_main())
+
+    print("help")
+    # main()
